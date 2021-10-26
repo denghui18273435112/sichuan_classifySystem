14c25bd247cb9e38dfc82b2e26bc16413b267aea
denghui <314983713@qq.com>
2021-10-26 17:52:11 +0800

    根据ip生成测试报告

--- run.py
+++ run.py
@@ -1,15 +1,12 @@
 #主运行
 import pytest
 from configs.path import result_path,allure_reportt_path
+import socket
 import os
 if __name__ == '__main__':
-    a =True
-    if a==True:
-        pytest.main(["-s","test_case","--alluredir", result_path])
-    elif a==False:
-        pytest.main(["-s","test_case/test_all.py","--alluredir", result_path])
-    # os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
-    # os.system("allure serve {}".format(result_path))
-
-
+    pytest.main(["-s","test_case","--alluredir", result_path])
+    compute_name = socket.getfqdn(socket.gethostname())
+    if compute_name == "SZPC065.zzldomain.com" or socket.gethostbyname(compute_name) == "192.168.1.66":
+        os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
+        os.system("allure serve {}".format(result_path))
 
