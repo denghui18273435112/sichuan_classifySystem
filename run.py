55d61dae71f4764c762eeb34f4d14f225a26cf12
denghui <314983713@qq.com>
2021-10-26 11:13:55 +0800

    新增用例123

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         pytest.main(["-s","test_case","--alluredir", result_path])
     elif a==False:
         pytest.main(["-s","test_case/test_all.py","--alluredir", result_path])
-    #os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
+    # os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
     # os.system("allure serve {}".format(result_path))
 
 
