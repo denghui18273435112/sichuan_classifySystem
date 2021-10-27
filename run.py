cd36d5489047d92ad8a211ab84739b9cc878aae9
denghui <314983713@qq.com>
2021-10-27 13:53:01 +0800

    测试 github+jenkins 自动构建

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     compute_name = socket.getfqdn(socket.gethostname())
     if compute_name == "SZPC065.zzldomain.com" or socket.gethostbyname(compute_name) == "192.168.1.66":
         os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
-        os.system("allure serve {}".format(result_path))
+        #os.system("allure serve {}".format(result_path))
 
