5cdb50c6326b19af42eb3ff7beab5466c8f53ba4
denghui <314983713@qq.com>
2021-10-27 14:01:14 +0800

    测试自动构建

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import os
 if __name__ == '__main__':
     pytest.main(["-s","test_case","--alluredir", result_path])
     compute_name = socket.getfqdn(socket.gethostname())
-    if compute_name == "SZPC065.zzldomain.com" or socket.gethostbyname(compute_name) == "192.168.1.66":
-        os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
-        os.system("allure serve {}".format(result_path))
+    # if compute_name == "SZPC065.zzldomain.com" or socket.gethostbyname(compute_name) == "192.168.1.66":
+    #     os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
+    #     os.system("allure serve {}".format(result_path))
 
