fb1b8f4bb2be906fc22fee2271572d7d84b71942
denghui <314983713@qq.com>
2021-11-05 16:59:12 +0800

    202111051659

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     pytest.main(["-s","test_case","--alluredir", result_path])
     compute_name = socket.getfqdn(socket.gethostname())
     print(os.path.dirname(os.path.abspath(__file__)))
-    if os.path.dirname(os.path.abspath(__file__))=="G:\pycharm\script\sichuan_classifySystem":
-        os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
-        os.system("allure serve {}".format(result_path))
+    # if os.path.dirname(os.path.abspath(__file__))=="G:\pycharm\script\sichuan_classifySystem":
+    #     os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
+    #     os.system("allure serve {}".format(result_path))
 
 
