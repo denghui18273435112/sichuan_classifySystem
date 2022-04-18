902eefa7615db2d3442a4bc30b586a375af33a11
denghui18273435112 <314983713@qq.com>
2022-04-18 15:14:42 +0800

    注释

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #主运行
 import pytest
 from configs.path import result_path,allure_reportt_path
-import os
+# import os
 if __name__ == '__main__':
     pytest.main(["-s","./test_case/role_association/test_all.py",
                    #"-m", "not(login or no)",
