aec3d3f8192d184d12860f1bce4ff1c1fda45e84
denghui18273435112 <314983713@qq.com>
2022-05-06 15:35:24 +0800

    20220506

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 #主运行
 import pytest
 from configs.path import result_path,allure_reportt_path
-# import os
+import os
 if __name__ == '__main__':
     pytest.main(["-s","./test_case/role_association/test_all.py",
                    #"-m", "not(login or no)",
                    # "-m", "test",
                  "--alluredir", result_path])
-    # os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
-    # os.system("allure serve {}".format(result_path))
+    os.system("allure generate {0} -o {1} --clean".format(result_path, allure_reportt_path))
+    os.system("allure serve {}".format(result_path))
 
